b658a55c0dacf1cfdf3e7c8d531d3ecafa2c0597
Xiao Xiao <xiao@weecology.org>
2015-06-08 15:44:53 +0200

    Add function to do the inital t-tests. \n Currently looks pretty messy.

--- scripts/sketch_functions.R
+++ scripts/sketch_functions.R
@@ -36,5 +36,52 @@ get_sample_stats = function(dat_in){
   return(dat_out)
 }
 
-initial_tests_S_N = function(dat_sample, )
+initial_tests_S_N = function(dat_sample, parametric, plot){
+  ## This function takes the output from get_sample_stats() and perform tests on whether samples differ 
+  ## by treatments. The test will be ANoVA if treatment is categorical, linear regression if it is 
+  ## continuous.
+  ## Inputs: 
+  ## dat_sample: output from get_sample_stats(), with 6 columns and one sample in each row.
+  ## parametric: If TRUE, p-value is read directly from the output of lm(). If FALSE, p-value is 
+  ##     computed by shuffling the treatment labels 1,000 times. Note that when the sample size is
+  ##     low there may not be 1,000 unique permutations of the treatment labels.
+  ## plot: If TRUE, a 2*3 plot is created to illustrate the comparisons of the five variables.\
+  ## Output:
+  ## A list of the five p-values: p_N, p_obsS, p_PIE, p_rareS, p_MIHS
+  p_vec = c()
+  f_vec = c()
+  for (i in 3:7){
+    model = summary(lm(dat_sample[, i]~dat_sample[, 2]))
+    f = model$fstat
+    f_vec = c(f_vec, f[1])
+    p_vec = c(p_vec, pf(f[1], f[2], f[3], lower = F))
+  }
+  if (parametric == F) { # Non-parametric test
+    f_frame = as.data.frame(matrix(nrow = 1000, ncol = 5))
+    for (j in 1:1000){
+      dat_rand = dat_sample
+      dat_rand$treatment = sample(dat_rand$treatment, length(dat_rand$treatment))
+      for (i in 3:7){
+        model = summary(lm(dat_rand[, i]~dat_rand[, 2]))
+        f = model$fstat
+        f_frame[j, i-2] = f[1]
+      }
+    }
+    for (i in 1:5){
+      p_vec = c(p_vec, length(f_frame[f_frame[, i]>f_vec[i], i]) / 1000)
+    }
+  }
+  if (plot == T){
+    # Currently it is assumed that treatment has distinct levels.
+    par(mfrow = c(2, 3))
+    col_names = c('N', 'Observed S', 'PIE', 'Rarefied S', 'MIH delta-S')
+    for (i in 1:5){
+      boxplot(dat_sample[, i + 2] ~ dat_sample[, 2], main = col_names[i],
+              las = 2)
+      mtext(paste('p=', p_vec[i], sep = ''), cex = 0.8)
+    }
+  }
+  return(list(p_N = p_vec[1], p_obs = p_vec[2], p_PIE = p_vec[3], p_rareS = p_vec[4], 
+              p_MIHS = p_vec[5]))
+}
 
