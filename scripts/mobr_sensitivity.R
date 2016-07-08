b551007f7af3c1bb8d79770d8c0c9e02278cdbd9
Xiao Xiao <xiao@weecology.org>
2016-07-08 10:11:13 +0200

    Create script for MOBR sensitivity test.

---
+++ scripts/mobr_sensitivity.R
@@ -0,0 +1,5 @@
+# Sensitivity test for Mobr functions
+# using Felix May's MoBspatial package
+
+library(MoBspatial)
+#library(mobr) # This doesn't work at the moment; will have to wait until the mobr package is built
