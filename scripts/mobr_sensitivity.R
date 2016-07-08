ff0193075bfabdf1e28e75e78948bc3935aa852c
Xiao Xiao <xiao@weecology.org>
2016-07-08 13:28:39 +0200

    Modify function for better output.

--- scripts/mobr_sensitivity.R
+++ scripts/mobr_sensitivity.R
@@ -4,28 +4,30 @@
 library(MoBspatial)
 #library(mobr) # This doesn't work at the moment; will have to wait until the mobr package is built
 
-# This function draws samples (plots) from a simulated community by dividing the domain up into plots.
-# Inputs:
-# sim_comm - a community simulated by Sim.Thomas.Community() or Sim.Poisson.Community()
-#   It has three columns: the X, Y coordinates of an individual and its SpecID
-# sqrt_N - sqrt(number of samples). The x- and y- coordinates of the domain will be divided
-#   by this number to create the plots.
-# xmax, ymax - size of the simulated community in X & Y coordinates
-# Output:
-# A list with two components:
-# $comm: A plot (sample) * species matrix. Species are allowed to have all zero values.
-# $coords: (x, y) coordinates at the center of each plot
 
-sample_comm = function(sim_comm, sqrt_N, xmax = 1, ymax = 1){
+# Simulate a community using Sim.Thomas.Community() and wrangle it into a list
+#   which can later be combined to create the comm object for mobr.
+# Inputs:
+# S, N, cv, sigma - parameters to simulate the community. xmax and ymax are fixed at 1.
+# sqrt_numplots - sqrt(number of plots) within the domain. This is how the domain will be divided.
+# Ouput:
+# A list with three components:
+# $comm - plot by species matrix
+# $coords - coordinates of the plots
+# $params - parameters used to create the simulation, including S, N, cv (shape of the SAD),
+#   and sigma (spatial aggregation)
+sim_comm_single_pars = function(S, N, cv, sigma, sqrt_numplots){
+  sim_comm = Sim.Thomas.Community(S, N, cv, sigma)
   out = list()
-  out$comm = matrix(NA, sqrt_N^2, nlevels(sim_comm$SpecID))
-  out$coords = matrix(NA, sqrt_N^2, 2)
-  x_span = xmax / sqrt_N
-  y_span = ymax / sqrt_N
+  out$comm = matrix(NA, sqrt_numplots^2, nlevels(sim_comm$SpecID))
+  out$coords = matrix(NA, sqrt_numplots^2, 2)
+  x_span = 1 / sqrt_numplots
+  y_span = 1 / sqrt_numplots
+  
   nrow = 1
-  for (i in 1:sqrt_N){
+  for (i in 1:sqrt_numplots){
     xlim = c(i-1, i) * x_span
-    for (j in 1:sqrt_N){
+    for (j in 1:sqrt_numplots){
       ylim = c(j-1, j) * y_span
       cond = which(sim_comm$X >= xlim[1] & sim_comm$X <= xlim[2] &
                      sim_comm$Y >= ylim[1] & sim_comm$Y <= ylim[2])
@@ -36,6 +38,10 @@ sample_comm = function(sim_comm, sqrt_N, xmax = 1, ymax = 1){
       nrow = nrow + 1
     }
   }
+  
+  out$coords = data.frame(out$coords)
+  names(out$coords) = c('x', 'y')
+  out$params = data.frame(t(c(S, N, cv, sigma)))
+  names(out$params) = c('S', 'N', 'cv', 'sigma')
   return(out)
 }
-
