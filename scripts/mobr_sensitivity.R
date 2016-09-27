67ebb21d4e33864e798201c526fbf1f5fb059a20
Xiao Xiao <xiao@weecology.org>
2016-09-27 17:26:22 -0400

    Modify sim_comm_single_pars() to be consistent with change in SAD.

--- scripts/mobr_sensitivity.R
+++ scripts/mobr_sensitivity.R
@@ -11,8 +11,7 @@ source('C:\\Users\\Xiao\\Documents\\GitHub\\mobr\\R\\mobr.R')
 # The set.seed steps ensure that the underlying distribution remains completely 
 #     unchanged when the shape parameters are the same
 SAD.lognorm <- function(S.pool, N.local, mean.abund = 100, cv.abund = 1,
-                        rand.seed = 10)
-{
+                        rand.seed = 10){
     sd.abund <- mean.abund*cv.abund
     
     sigma1 <- sqrt(log(sd.abund^2/mean.abund^2 +1))
@@ -42,44 +41,48 @@ SAD.lognorm <- function(S.pool, N.local, mean.abund = 100, cv.abund = 1,
 # N is N_local (number of individuals for the local group)
 # Because of the sampling process, the local group may not have all S species.
 # sqrt_numplots - sqrt(number of plots) within the domain. This is how the domain will be divided.
+# rand.seed - random seed for SAD, which controls whether the underlying metacommunity
+#               SAD remains unchanged or not.
 # Ouput:
 # A list with three components:
 # $comm - plot by species matrix
 # $coords - coordinates of the plots
 # $params - parameters used to create the simulation, including S, N, cv (shape of the SAD),
 #   and sigma (spatial aggregation)
-sim_comm_single_pars = function(S, N, cv, sigma, sqrt_numplots){
-  sim_comm = Sim.Thomas.Community(S, N, cv, sigma)
-  out = list()
-  out$comm = matrix(0, sqrt_numplots^2, S)
-  names(out$comm) = sapply(1:S, function(x) paste('species', as.character(x), sep = ''))
-  out$coords = matrix(NA, sqrt_numplots^2, 2)
-  x_span = 1 / sqrt_numplots 
-  y_span = 1 / sqrt_numplots 
+sim_comm_single_pars = function(S, N, cv, sigma, sqrt_numplots, rand.seed = 10){
+    sim_sad = SAD.lognorm(S, N, cv.abund = cv, rand.seed = rand.seed)
+    sim_comm = Sim.Thomas.Coords(sim_sad$abund, sigma)
+    out = list()
+    out$comm = matrix(0, sqrt_numplots^2, S)
+    names(out$comm) = sapply(1:S, function(x) paste('species', as.character(x), 
+                                                    sep = ''))
+    out$coords = matrix(NA, sqrt_numplots^2, 2)
+    x_span = 1 / sqrt_numplots 
+    y_span = 1 / sqrt_numplots 
   
-  nrow = 1
-  for (i in 1:sqrt_numplots){
-    xlim = c(i-1, i) * x_span
-    for (j in 1:sqrt_numplots){
-      ylim = c(j-1, j) * y_span
-      cond = which(sim_comm$X >= xlim[1] & sim_comm$X <= xlim[2] &
-                     sim_comm$Y >= ylim[1] & sim_comm$Y <= ylim[2])
-      comm_plot = sim_comm[cond, ]
-      plot_sad = data.frame(table(comm_plot$SpecID))
-      for (k in 1:nrow(plot_sad)){
-        out$comm[nrow, which(names(out$comm) == as.character(plot_sad[k, 1]))] = 
-          plot_sad[k, 2]
-      }
-      out$coords[nrow, ] = c(mean(xlim), mean(ylim))
-      nrow = nrow + 1
+    nrow = 1
+    for (i in 1:sqrt_numplots){
+        xlim = c(i-1, i) * x_span
+        for (j in 1:sqrt_numplots){
+            ylim = c(j-1, j) * y_span
+            cond = which(sim_comm$X >= xlim[1] & sim_comm$X <= xlim[2] &
+                             sim_comm$Y >= ylim[1] & sim_comm$Y <= ylim[2])
+            comm_plot = sim_comm[cond, ]
+            plot_sad = data.frame(table(comm_plot$SpecID))
+            for (k in 1:nrow(plot_sad)){
+                out$comm[nrow, which(names(out$comm) == as.character(plot_sad[k, 1]))] = 
+                plot_sad[k, 2]
+            }
+            out$coords[nrow, ] = c(mean(xlim), mean(ylim))
+            nrow = nrow + 1
+        }
     }
-  }
   
-  out$coords = data.frame(out$coords)
-  names(out$coords) = c('x', 'y')
-  out$params = data.frame(t(c(S, N, cv, sigma)))
-  names(out$params) = c('S', 'N', 'cv', 'sigma')
-  return(out)
+    out$coords = data.frame(out$coords)
+    names(out$coords) = c('x', 'y')
+    out$params = data.frame(t(c(S, N, cv, sigma)))
+    names(out$params) = c('S', 'N', 'cv', 'sigma')
+    return(out)
 }
 
 # Combine multiple simulations with different parameters into a comm object
