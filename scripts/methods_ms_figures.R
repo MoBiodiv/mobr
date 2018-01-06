223b00927bbb17edfc82242d1ddcae62c3dec660
Dan McGlinn <danmcglinn@gmail.com>
2018-01-05 23:24:51 -0500

    Add script for generating the methods paper figures

---
+++ scripts/methods_ms_figures.R
@@ -0,0 +1,35 @@
+library(mobr)
+
+data(inv_comm)
+data(inv_plot_attr)
+inv_mob_in= make_mob_in(inv_comm, inv_plot_attr)
+
+# run analyses
+inv_mob_stats = get_mob_stats(inv_mob_in, 'group', n_perm=999)
+inv_mob_out = get_delta_stats(inv_mob_in, 'group', ref_group='uninvaded',
+                           type='discrete', log_scale=TRUE, n_perm=999,
+                           overall_p=TRUE)
+
+# create graphics
+pdf('./figs/inv_mob_stats.pdf', height=7*0.5)
+plot(inv_mob_stats)
+# additional plot of f0 without outline
+plot(inv_mob_stats, 'f_0', outline=F)
+dev.off()
+
+pdf('./figs/inv_mob_stats_multi.pdf', width=7*1.25, height=7*2)
+plot(inv_mob_stats, multi_panel = T)
+plot(inv_mob_stats, multi_panel = T, outline = F)
+dev.off()
+
+pdf('./figs/inv_delta_stats.pdf')
+plot(inv_mob_out, 'invaded', 'uninvaded')
+dev.off()
+
+col = c("#FFB3B5", "#78D3EC", "#6BDABD", "#C5C0FE",
+        "#E2C288", "#F7B0E6", "#AAD28C")
+pdf('./figs/inv_mob_stacked.pdf')
+overlap_effects(inv_mob_out, 'invaded', col = col[c(1,2,4)])
+overlap_effects(inv_mob_out, 'invaded', 'stacked', prop=T,
+                col = col[c(1,2,4)])
+dev.off()
