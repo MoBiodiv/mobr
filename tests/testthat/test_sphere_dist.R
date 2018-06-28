0dc92947a1fe840796bc68f8630ec4f79c1f02cd
Caroline Oliver <carolineoliver@Carolines-MBP.knology.net>
2018-06-28 15:26:35 -0400

    This commit adds/alters files and folders to the mobr repository so that automated testing may be performed. More detailed instructions for writing/running test files can be found on the main MoBiodiv/mobr page in the wiki section.
    The DESCRIPTION file has been alterd to include 'testthat' under the 'Suggests' portion. This is done automatically when the package is instantiated.
    
    2 folders and 2 files have also been added. The main folder that houses the other folder and files is named 'tests'. The tests folder has within it a testthat.R file that was auto-added when the package was initialized, as well as, the second folder called 'testthat'. Within the 'testthat' folder is where the testing files for the various mobr functions will be housed. There is currently one additional file in the testthat folder named 'test_sphere_dist.R'. This file contains the basic set up for a test file and one preliminary expectation that tests if the sphere_dist function is equal to itself. This test should always pass. This test file has been run successfully using the source()/test_file() method.

---
+++ tests/testthat/test_sphere_dist.R
@@ -0,0 +1,16 @@
+context("Testing of the sphere_dist function")
+
+test_that("matrix values are accurate provided 2 columns of lat and longs data", {
+  
+  # Remove first column of 'plotunique' data from the Fire_lat_longs data file
+  test_fire_lat_longs = Fire_lat_longs[ ,2:3]
+  
+  # Compute sphere_dist matrix to be tested against- assume correct values are calcuated
+  test_dist1 = sphere_dist(test_fire_lat_longs)
+  
+  # EXPECTATION 1
+  # Compute sphere_dist using Fire_lat_longs data and compare it to the test_dist1 matrix created above
+  # This test should always pass since both distance matrices are calculated from the same data
+  expect_equal(sphere_dist(test_fire_lat_longs), test_dist1)
+  
+})
