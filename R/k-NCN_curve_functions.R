043df117303f70c0343a7ef023ef40538aa918b5
Thore Engel <thore.engel@idiv.de>
2019-02-22 17:16:13 +0100

    Add repetitions argument

--- R/k-NCN_curve_functions.R
+++ R/k-NCN_curve_functions.R
@@ -38,16 +38,18 @@ centroid_accumulate<-function(x,focal_sample= 1, n=NULL, coords=NULL){
 #' This function accumulates samples according their proximity to all previously included samples (their centroid) as opposed
 #' to the proximity to the inital focal sample. This ensures that included samples mutually close to each other and not all over the place.
 #' 
-#' Internally the function constructs one curve per sample whereby each sample serves as the initial sample once. Finally, the average curve is returned.
+#' Internally the function constructs one curve per sample whereby each sample serves as the initial sample repetition times. Finally, the average curve is returned.
+#'
 #' @param x a mob_in object or a sites x species matrix
 #' @param n number of sites to include. 
 #' @param coords spatial coordinates of the samples. If x is a mob_in object, the fuction uses its "spat" table as coords.
+#' @param repetitions Number of times to repeat the procedure. Useful in situations where there are many ties in the distance matrix.
 #'
 #' @return a numeric vector of estimated species richness
 #' @export
 #'
 #' @examples
-kNCN_average<-function(x, n=NULL, coords=NULL){
+kNCN_average<-function(x, n=NULL, coords=NULL, repetitions=1){
   if(class(x)== "mob_in"){
     sites=x$comm
     coords=x$spat
@@ -55,7 +57,7 @@ kNCN_average<-function(x, n=NULL, coords=NULL){
   rownames(sites)=NULL
   rownames(coords)=NULL
   if(is.null(n)) n=dim(sites)[1]
-  samples=1:n
+  samples=rep(1:n,each=repetitions)
   require(pbapply)
   out<-pbsapply(samples,function(sample)  centroid_accumulate(x = x,focal_sample = sample, coords = coords) )
   return(rowMeans(out))
