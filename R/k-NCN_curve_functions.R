2449242031f95b246b97dccace78f6c10c74704d
Dan McGlinn <danmcglinn@gmail.com>
2019-06-10 16:49:57 -0400

    Refactored kNCN code
    * enforced package style
    * improved documentation
    * cleaned up progress bar output
    * added parallel computaiton option
    * specified depenancies correctly
    * added examples

--- R/k-NCN_curve_functions.R
+++ R/k-NCN_curve_functions.R
@@ -1,36 +1,51 @@
-# Internal function for kNCN_average (). k-NCN algorithm starting with a specified focal sample
+#' Internal function used by kNCN_average to compute the k-NCN algorithm
+#' starting with a specified focal sample
+#' 
+#' @param x a mob_in object or a community site x species matrix
+#' @param focal_sample an integer from 1 to the number of samples of x that 
+#' specifies which sample to start with. Defaults to 1
+#' @param n the number of samples to accumulate, defaults to NULL in which 
+#' case all samples are accumulated
+#' @param coords the spatial coordinates of the samples of x
+#' 
+#' @importFrom sp SpatialPoints
+#' @importFrom rgeos gCentroid
+#' @importFrom pracma distmat
+#' 
+#' @keywords internal
+centroid_accumulate = function(x, focal_sample = 1, n = NULL, coords = NULL) {
 
-centroid_accumulate<-function(x,focal_sample= 1, n=NULL, coords=NULL){
-  require(rgeos)
-  require(maptools)
-  require(pracma)
-  require(sp)
-  if(class(x)== "mob_in"){
-      sites=x$comm
-      coords=x$spat
-  }else{sites=x}
-  sites<-as.matrix(sites)
-  coords<-as.matrix(coords)
-  rownames(sites)=NULL
-  rownames(coords)=NULL
-  if(is.null(n)) n=dim(sites)[1]
-  included=focal_sample
-  pool= colSums(sites[included,,drop=F])
-  S_accumulated=as.numeric()
-  sp_object<-SpatialPoints(coords)
-  candidates<-cbind(coords, 1:dim(sites)[1])
-  for(i in 1:n){
-    S_accumulated[i]<-vegan::specnumber(pool)
-    if(i==n) break() 
-    accumulated_plots<-sp_object[included,]
-    centroid<-gCentroid(accumulated_plots)@coords
-    candidates2<-candidates[-included,, drop=F]
-    closest<-candidates2[order(distmat(centroid, candidates2[,-3, drop=F]),  runif(dim(candidates2)[1]) )[1],3,drop=T]
-    included=c(included, closest)
-    pool<- pool + sites[closest,]
-    
-  }
-  return(S_accumulated)
+    if (class(x) == "mob_in") {
+        comm = x$comm
+        coords = x$spat
+    } else {
+        comm = x
+    }
+    comm = as.matrix(comm)
+    coords = as.matrix(coords)
+    rownames(comm) = NULL
+    rownames(coords) = NULL
+    if (is.null(n)) 
+        n = nrow(comm)
+    included = focal_sample
+    pool = colSums(comm[included, , drop = F])
+    S_accumulated = rep(0, n)
+    S_accumulated[1] = sum(pool > 0)
+    sp_object = sp::SpatialPoints(coords)
+    candidates = cbind(coords, 1:n)
+    for (i in 1:(n - 1)) {
+        accumulated_plots = sp_object[included, ]
+        centroid = rgeos::gCentroid(accumulated_plots)@coords
+        candidates2 = candidates[-included, , drop = F]
+        spat_dists = pracma::distmat(centroid, candidates2[ , -3, drop = F])
+        random_tie_breaker = runif(nrow(candidates2))
+        nearest_index = order(spat_dists, random_tie_breaker)[1]
+        nearest_plot = candidates2[nearest_index, 3, drop = T]
+        included = c(included, nearest_plot)
+        pool = pool + comm[nearest_plot, ]
+        S_accumulated[i + 1] = sum(pool > 0)
+    }
+    return(S_accumulated)
 }
 
 #' Construct spatially constrained sample-based rarefaction (sSBR) curve using the k-Nearest-Centroid-neighbour (k-NCN) algorithm 
@@ -40,29 +55,59 @@ centroid_accumulate<-function(x,focal_sample= 1, n=NULL, coords=NULL){
 #' 
 #' Internally the function constructs one curve per sample whereby each sample serves as the initial sample repetition times. Finally, the average curve is returned.
 #'
-#' @param x a mob_in object or a sites x species matrix
+#' @param x a mob_in object or a community site x species matrix
 #' @param n number of sites to include. 
-#' @param coords spatial coordinates of the samples. If x is a mob_in object, the fuction uses its "spat" table as coords.
+#' @param coords spatial coordinates of the samples. If x is a mob_in object, the fuction uses its 'spat' table as coords.
 #' @param repetitions Number of times to repeat the procedure. Useful in situations where there are many ties in the distance matrix.
-#'
+#' @param no_pb binary, if TRUE then a progress bar is not printed, defaults to TRUE
+#' 
+#' @inheritParams pbapply::pbreplicate
+#' 
 #' @return a numeric vector of estimated species richness
+#' 
+#' @importFrom pbapply pbsapply pboptions
+#' 
 #' @export
 #'
 #' @examples
-kNCN_average<-function(x, n=NULL, coords=NULL, repetitions=1){
-  if(class(x)== "mob_in"){
-    sites=x$comm
-    coords=x$spat
-  }else{sites=x}
-  rownames(sites)=NULL
-  rownames(coords)=NULL
-  if(is.null(n)) n=dim(sites)[1]
-  samples=rep(1:n,each=repetitions)
-  require(pbapply)
-  out<-pbsapply(samples,function(sample)  centroid_accumulate(x = x,focal_sample = sample, coords = coords) )
-  return(rowMeans(out))
+#' data(inv_comm)
+#' data(inv_plot_attr)
+#' inv_mob_in = make_mob_in(inv_comm, inv_plot_attr, coords = c('x', 'y'))
+#' kNCN_average(inv_mob_in)
+#' \donttest{
+#' # Not run: 
+#' # parallel evaluation using the parallel package 
+#' # run in parallel
+#' library(parallel)
+#' cl = makeCluster(2L)
+#' clusterEvalQ(cl, library(mobr))
+#' clusterExport(cl, 'inv_mob_in')
+#' S_kNCN = kNCN_average(inv_mob_in, cl=cl)
+#'
+#' stopCluster(cl)
+#' }
+kNCN_average = function(x, n = NULL, coords = NULL, repetitions = 1, 
+                        no_pb = TRUE, cl = NULL) {
+    if (class(x) == "mob_in") {
+        sites = x$comm
+        coords = x$spat
+    } else {
+        sites = x
+    }
+    rownames(sites) = NULL
+    rownames(coords) = NULL
+    if (is.null(n)) 
+        n = dim(sites)[1]
+    samples = rep(1:n, each = repetitions)
+    op <- pbapply::pboptions()
+    if (no_pb)
+        pbapply::pboptions(type = 'none')
+    out = pbapply::pbsapply(samples, function(sample) 
+                            centroid_accumulate(x = x, focal_sample = sample, 
+                                                coords = coords), cl = cl)
+    pbapply::pboptions(op) # reset options to default
+    return(rowMeans(out))
 }
-
 
 #' Compare all sample-based curves (random, spatiallyconstrained-k-NN, spatially constrained-k-NCN)
 #'
@@ -74,14 +119,19 @@ kNCN_average<-function(x, n=NULL, coords=NULL, repetitions=1){
 #' @export
 #'
 #' @examples
-compare_curves<-function(x){
-  
-  centroid_curve<-kNCN_average(x)
-  spatcurve<-rarefaction(x,method = "spat")
-  samplecurve<-rarefaction(x,method = "samp")
-  plot(samplecurve, type= "l", xlab="Samples", ylab="Expected species richness")
-  lines(centroid_curve,col=2)
-  lines(spatcurve, col=3)
-  legend("bottomright", legend = c("sample based", "k-NCN","k-NN"), col=1:3, lty=1)
+#' data(inv_comm)
+#' data(inv_plot_attr)
+#' inv_mob_in = make_mob_in(inv_comm, inv_plot_attr, coords = c('x', 'y'))
+#' compare_curves(inv_mob_in)
+compare_curves = function(x) {
+    
+    centroid_curve = kNCN_average(x)
+    spatcurve = rarefaction(x, method = "spat")
+    samplecurve = rarefaction(x, method = "samp")
+    plot(samplecurve, type = "l", xlab = "Samples", ylab = "Expected species richness")
+    lines(centroid_curve, col = 2)
+    lines(spatcurve, col = 3)
+    legend("bottomright", legend = c("sample based", "k-NCN", "k-NN"), col = 1:3, 
+        lty = 1)
 }
 
