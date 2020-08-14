90f09e4f95f149656a70db57890e3c9eadbfed3c
Dan McGlinn <danmcglinn@gmail.com>
2020-08-14 15:22:19 -0400

    Change pkg documentation per win_test warning

--- R/mobr-package.R
+++ R/mobr-package.R
@@ -1,12 +1,10 @@
-#' mobr: measurement of biodiversity across scales
+#' @title Measurement of Biodiversity in R 
 #' 
-#' @section About:
-#' 
-#' The primary aim of this package is to provide ecologist's tools to examine 
-#' changes in biodiversity across spatial scales.  Additionally the package
-#' provides a method to examine how a factor mediates species richness via its
-#' effects on different aspects of community structure: total abundance,
-#' species commonness, and spatial aggregation of conspecifics. 
+#' @description  The primary aim of this package is to provide ecologist's tools
+#'   to examine changes in biodiversity across spatial scales.  Additionally, the
+#'   package provides a method to examine how a factor mediates species richness
+#'   via its effects on different aspects of community structure: total
+#'   abundance, species commonness, and spatial aggregation of conspecifics.
 #' 
 #' @name mobr
 #' @docType package
