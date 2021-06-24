d9e99674d978bb4070c140c77ec49336380aa341
Daniel McGlinn <danmcglinn@gmail.com>
2021-06-24 12:48:22 -0400

    Add small beta diversity demonstration script and associated pdf

---
+++ vignettes/beta_div_demo.R
@@ -0,0 +1,32 @@
+#' ---
+#' title: "beta diversity demonstration"
+#' author: Dan McGlinn
+#' output: pdf_document
+#' ---
+#' 
+
+#+ setup, include=FALSE, eval=FALSE
+install.packages(devtools)
+library(devtools)
+install_github('mobiodiv/mobr', ref = 'dev')
+
+#' load mobr and example data
+library(mobr)
+data(inv_comm)
+
+#' Calculate whittaker's beta and beta Coverage
+calc_comm_div(inv_comm[1:2, ], 'S')
+
+#' Calculate beta S_PIE
+calc_comm_div(inv_comm[1:2, ], 'S_PIE')
+
+#' Calculate beta S_n for 20 individuals
+calc_comm_div(inv_comm[1:2, ], 'S_n', effort = 20)
+
+#' More than two sites can be used at a time
+calc_comm_div(inv_comm[1:10, ], 'S')
+
+#' It is also possible to just calculate beta diversity
+#' but it is generally not recommended to examine beta 
+#' without reference to alpha and gamma diversity
+calc_beta_div(inv_comm[1:10, ] , 'S')
